60c09982f9b61bcda752177ade7c31397338e79d
Dennys Izhyck <denysizhuk@gmail.com>
2022-06-29 08:02:41 +0300

    Initial Commit

---
+++ FilmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by Denys on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
